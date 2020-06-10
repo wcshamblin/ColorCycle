1c720bcf009f8ea2cfdca26bbc38bc2544576478
wcshamblin <will@shamblin.org>
2020-06-10 12:04:25 -0400

    Added stupid GTK bs

--- colorcycle.py
+++ colorcycle.py
@@ -6,8 +6,6 @@ import dbus
 # ps.add_argument("sublprefs", type=str, help='Path to Sublime Text\'s preferences file')
 # args = ps.parse_args()
 
-ts = "dark"
-
 # Sublime Text
 sublfile = "/home/wcs/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
 subllight = "Packages/Solarized Color Scheme/Solarized (light).sublime-color-scheme"
@@ -25,6 +23,12 @@ urxvtdark = "/home/wcs/.urxvt/themes/solarized-dark"
 walldict = {0: {"dark": "/home/wcs/Pictures/Wallpapers/darkright.jpg", "light": "/home/wcs/Pictures/Wallpapers/lightright.jpg"},
             1: {"dark": "/home/wcs/Pictures/Wallpapers/darkleft.jpg", "light": "/home/wcs/Pictures/Wallpapers/lightleft.jpg"}
             }
+
+# GTK
+# gtkpath = "/home/wcs/.config/gtk-3.0/settings.ini"
+
+
+ts = "dark"
 
 # Sublime Text
 sublprefs = open(sublfile, "r+")
@@ -49,6 +53,7 @@ else:
 system("kill -1 $(pidof urxvtd)")
 
 
+
 # Wallpaper
 def setwallpaper(filepath, screennum):
     jscript = """
@@ -67,3 +72,21 @@ for i in range(0, len(walldict)):
         setwallpaper(walldict[i]["dark"], i)
     else:
         setwallpaper(walldict[i]["light"], i)
+
+
+# GTK retardation
+# gtkfile = open(gtkpath)
+# gtksettings=gtkfile.read()
+# if "gtk-application-prefer-dark-theme" not in gtksettings:
+#     if ts == "dark":
+#         gtksettings+="\ngtk-application-prefer-dark-theme=1"
+#     else:
+#         gtksettings+="\ngtk-application-prefer-dark-theme=0"
+# else:
+#     if ts == "dark":
+#         gtksettings = gtksettings.replace("gtk-application-prefer-dark-theme=0", "gtk-application-prefer-dark-theme=1")
+#     else:
+#         gtksettings = gtksettings.replace("gtk-application-prefer-dark-theme=1", "gtk-application-prefer-dark-theme=0")
+# gtkfile.seek(0)
+# gtkfile.write(gtksettings)
+# gtkfile.close()
